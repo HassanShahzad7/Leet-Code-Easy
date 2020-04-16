ecb99b0f32aaa69e33c76922e6f5a9a8786a0d2f
HassanShahzad7 <hshahzad.bscs17seecs@seecs.edu.pk>
2020-04-16 20:54:59 +0500

    Added Documentation

--- TwoSum.py
+++ TwoSum.py
@@ -1,9 +1,17 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
+	
+	# initializing an empty dictionary 
         hashmap = dict()
+
         for i in range(len(nums)):
+
+	    # if the number exists in hashmap as a key then return a tuple
             if nums[i] in hashmap.keys():
                 return (i, hashmap[nums[i]])
+	    
             else:
+		# setting diff equal to the difference of target and entries in the array
                 diff = target - nums[i]
+		# setting the dictionary key values to the loop variable
                 hashmap[diff] = i
